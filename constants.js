706c7a644aad0b37c8c39bff4e48288b575112e9
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-09-20 16:53:36 +0200

    changes

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 module.exports = {
     appName: 'Tienda Panda BK',
     server: {
-        port: process.env.PORT || 8080
+        port: process.env.PORT || 8080,
+        host: process.env.HOST || 'localhost'
     },
     mongo: {
         url: process.env.url,
